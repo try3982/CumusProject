b6db263037fd8ba6be5fc7fffd9b69ae73354a2c
WonJin <try3982@kakao.com>
2025-02-11 22:30:58 +0900

    dependency 설정

---
+++ src/main/java/com/zerobase/cms/CumusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumusProjectApplication.class, args);
+	}
+
+}
